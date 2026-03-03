b6ab54754b5ab9099add6a66139401decd60d3ec
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-03-03 22:36:10 +1100

    targa image setup

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main(int argc, char **argv)
     TGAImage image(100, 100, TGAImage::RGB);
     image.set(52, 41, red);
     image.flip_vertically();
-    image.write_tga_file("output.tga")
+    image.write_tga_file("output.tga");
+    return 0;
 }
